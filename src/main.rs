38a2e54693cd0e6c053ed3f1f01adaf2ae31cfc8
Shaurya Goel <shoray.goel@gmail.com>
2025-07-05 12:14:12 +0530

    Use tokio to handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
-#![allow(unused_imports)]
-use std::{
-    io::{BufRead, Read, Write},
-    net::TcpListener,
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+async fn process(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 1024];
+    while let Ok(bytes_read) = stream.read(&mut buf).await {
+        if bytes_read == 0 {
+            break;
+        }
+        stream.write_all(b"+PONG\r\n").await.unwrap();
+    }
+}
 
-                while let Ok(bytes_read) = stream.read(&mut buf) {
-                    if bytes_read == 0 {
-                        break;
-                    }
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        match listener.accept().await {
+            // The second item contains the IP and port of the new connection.
+            Ok((stream, _)) => {
+                // A new task is spawned for each inbound socket. The socket is
+                // moved to the new task and processed there.
+                tokio::spawn(async move {
+                    process(stream).await;
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
